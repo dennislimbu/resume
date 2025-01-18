3991b4047199750e9ec7641756adfaf776e6621b
Dennis Limbu <141339233+dennislimbu@users.noreply.github.com>
2025-01-18 21:59:00 +0000

    pdating footer and app.css

--- project-resume/src/components/Footer.js
+++ project-resume/src/components/Footer.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/Logo.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -10,15 +10,13 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+            <img src={logo} class="logo" alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/dennis-limbu/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2025. All Rights Reserved</p>
+            <p>Copyright 2025 Dennis Limbu. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
--- project-resume/src/components/Skills.js
+++ project-resume/src/components/Skills.js
@@ -1,16 +1,13 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -28,61 +25,47 @@ export const Skills = () => {
     }
   };
 
+  const skills = [
+    { name: "Web Development", percentage: 90 },
+    { name: "HTML5", percentage: 95 },
+    { name: "CSS3", percentage: 85 },
+    { name: "JavaScript", percentage: 80 },
+    { name: "Python", percentage: 70},
+    { name: "Django", percentage: 70 },
+    { name: "Heroku", percentage: 65 },
+    { name: "GitHub", percentage: 90 },
+    { name: "Git", percentage: 85 },
+    { name: "React", percentage: 80 }
+  ];
+
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>HTML5</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>CSS3</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>JavaScript</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Python</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Django</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Heroku</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Git Hub</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Git</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>React</h5>
-                            </div>
-                        </Carousel>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>As a software developer with a strong foundation in both front-end and back-end technologies, I have developed a diverse set of skills that enable me to build and maintain dynamic, user-friendly web applications. My technical expertise includes:</p>
+              <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                {skills.map((skill, index) => (
+                  <div className="item" key={index}>
+                    <h5>{skill.name}</h5>
+                    <CircularProgressbar
+                      value={skill.percentage}
+                      text={`${skill.percentage}%`}
+                      styles={buildStyles({
+                        textColor: "#fff",
+                        pathColor: "rgba(128, 0, 128, 0.8)", 
+                      })}
+                    />
+                  </div>
+                ))}
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
-  )
-}
+  );
+};
